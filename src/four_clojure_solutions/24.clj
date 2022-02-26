6fc2c9d0764ae337fac5811756669365756f7d4a
Alysa Chan <alysachansl@gmail.com>
2022-02-26 23:21:41 +0800

    Add question 24

---
+++ src/four_clojure_solutions/24.clj
@@ -0,0 +1,12 @@
+(ns four-clojure-solutions.24)
+
+;Write a function which returns the sum of a sequence of numbers.
+
+;(= (__ [1 2 3]) 6)
+;(= (__ (list 0 -2 5 5)) 8)
+;(= (__ #{4 2 1}) 7)
+;(= (__ '(0 0 -1)) -1)
+;(= (__ '(1 10 3)) 14)
+
+;My solution 1
+#(reduce + %)
