5870ce51ee671d9c2d55665f5a48768290a1b071
Alysa Chan <alysachansl@gmail.com>
2022-05-08 17:29:58 +0800

    Add comment

--- src/four_clojure_solutions/27.clj
+++ src/four_clojure_solutions/27.clj
@@ -2,6 +2,7 @@
 
 ;Write a function which returns true if the given sequence is a palindrome. Hint: "racecar" does not equal '(\r \a \c \e \c \a \r)
 
+; My solution
 (fn palindrome [x]
   (def t (vec (seq x)))
   (if (< (count t) 3) false)
